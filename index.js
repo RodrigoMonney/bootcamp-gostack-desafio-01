75f229bca416d4306200807cd1d9dc77329d2d10
Rodrigo Reis <rodrigo.monney@gmail.com>
2019-10-31 22:52:08 -0300

    refactor: change route '/project/:id' to '/projects/:id'

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const server = express()
 
 let projects = [{ id: 1, title: 'Sample project', tasks: [] }]
 
-server.get('/project/:id', (req, res) => {
+server.get('/projects/:id', (req, res) => {
   const projectId = req.params.id;
   const project = projects.filter(project => project.id == projectId)
 
@@ -12,7 +12,7 @@ server.get('/project/:id', (req, res) => {
     return res.json({ project })
   }
 
-  res.status(404).json({ error: `Project with id ${projectId} couldn't be found.` })
+  res.status(404).json({ error: `Project with id '${projectId}' couldn't be found.` })
 })
 
 server.listen(3000)
